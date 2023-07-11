26b9c2b197b1b8d7b1ed723509fdacfeacd1e3db
Lauren <halpertln@gmail.com>
2023-07-11 14:13:05 -0600

    complete lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,17 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Name is required')
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, number):
+        if len(number) != 10:
+            raise ValueError('Phone number is not ten digits')
+        return number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +38,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) <=250:
+            raise ValueError('Content length is too short')
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >=250:
+            raise ValueError('Summary length is too long')
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError('Invalid category')
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if 'Won\'t believe' not in title or 'Secret' not in title or 'Top' not in title or 'Guess' not in title:
+            raise ValueError('Title is not clickbait')
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
